530566700bf6064a6ef502d18c4f2b62c7e2aff8
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-01-30 23:07:27 -0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -1,98 +1,99 @@
+// Definição de uma lista de objetos representando diferentes pizzas
 let pizzaJson = [
   {
     id: 1,
     name: 'Cristo Redentor',
     img: 'images/pizza.png',
-    price: [20.00, 23.00, 25.00],
+    price: [20.00, 23.00, 25.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, camada dupla de mussarela e orégano'
+    description: 'Molho de tomate, camada dupla de mussarela e orégano' // Descrição da pizza
   },
   {
     id: 2,
     name: 'Pão de Açúcar',
     img: 'images/pizza2.png',
-    price: [21.00, 24.00, 26.00],
+    price: [21.00, 24.00, 26.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'
+    description: 'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano' // Descrição da pizza
   },
   {
     id: 3,
     name: 'Copacabana',
     img: 'images/pizza3.png',
-    price: [23.00, 26.00, 28.00],
+    price: [23.00, 26.00, 28.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'
+    description: 'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano' // Descrição da pizza
   },
   {
     id: 4,
     name: 'Ipanema',
     img: 'images/pizza4.png',
-    price: [25.00, 28.00, 30.00],
+    price: [25.00, 28.00, 30.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'
+    description: 'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano' // Descrição da pizza
   },
   {
     id: 5,
     name: 'Maracanã',
     img: 'images/pizza5.png',
-    price: [24.00, 27.00, 29.00],
+    price: [24.00, 27.00, 29.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'
+    description: 'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano' // Descrição da pizza
   },
   {
     id: 6,
     name: 'Lapa',
     img: 'images/pizza6.png',
-    price: [30.00, 33.00, 35.00],
+    price: [30.00, 33.00, 35.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'
+    description: 'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano' // Descrição da pizza
   },
   {
     id: 7,
     name: 'Arcos da Lapa',
     img: 'images/pizza7.png',
-    price: [27.00, 30.00, 32.00],
+    price: [27.00, 30.00, 32.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Mussarela, banana, canela e açúcar'
+    description: 'Mussarela, banana, canela e açúcar' // Descrição da pizza
   },
   {
     id: 8,
     name: 'Carnaval',
     img: 'images/pizza8.png',
-    price: [30.00, 32.00, 35.00],
+    price: [30.00, 32.00, 35.00], // Preços para diferentes tamanhos de pizza
     sizes: [
       '6 fatias',
       '8 fatias',
       '12 fatias'
     ],
-    description: 'Creme de leite, lascas de chocolate e morangos'
+    description: 'Creme de leite, lascas de chocolate e morangos' // Descrição da pizza
   }
 ];
